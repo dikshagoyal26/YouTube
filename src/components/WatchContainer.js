e0a4135fed3ca708fd0a72e19fbc815541466f45
Diksha <dg.dikshag2612@gmail.com>
2023-02-17 22:53:44 +0530

    managing nested comments

--- src/components/WatchContainer.js
+++ src/components/WatchContainer.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 import { closeMenu } from "../utils/appSlice";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchContainer = () => {
   const [searchParams] = useSearchParams();
@@ -15,16 +16,19 @@ const WatchContainer = () => {
   }, []);
 
   return (
-    <div className="px-5">
-      <iframe
-        width="1200"
-        height="600"
-        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
+    <div className="flex flex-col">
+      <div className="px-5">
+        <iframe
+          width="1200"
+          height="600"
+          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <CommentsContainer />
     </div>
   );
 };
